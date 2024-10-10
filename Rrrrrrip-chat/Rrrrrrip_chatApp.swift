0e9dc576108d5d6f60fcbf6cfa17f179ca0771f7
Your Name <your.email@example.com>
2024-10-10 12:50:12 +0900

    Initial Commit

---
+++ Rrrrrrip-chat/Rrrrrrip_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rrrrrrip_chatApp.swift
+//  Rrrrrrip-chat
+//
+//  Created by AW on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct Rrrrrrip_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
