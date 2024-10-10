1a6ddd7768cb1b48c339c309fb1f4257faa0ac92
Your Name <your.email@example.com>
2024-10-10 19:56:20 +0900

    新規ユーザー最終確認画面

---
+++ app/createUser/lastCheck.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import ImgPick from '../compData/imgPick';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../recoil';
+
+const LastCheck = () => {
+
+  const  userData = useRecoilValue(userState);
+
+  return (
+    <View style={s.container}>
+
+      <Text style={s.mainTitle}>最終確認</Text>
+      <Text style={s.mainText}>もう一度入力したプロフィールを確認しよう！</Text>
+
+      <ImgPick/>
+
+      <Text>{userData.name}</Text>
+      <Text>{userData.userId}</Text>
+      <Text>{userData.text}</Text>
+
+    </View>
+  );
+};
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  mainTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '80%',
+  },
+  mainText: {
+    fontSize: 14,
+    width: '80%',
+    marginBottom: 50,
+  },
+});
+
+export default LastCheck;
