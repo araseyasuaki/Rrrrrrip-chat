bcbb8868363b17db0b5f7829244617b367f48fd2
Your Name <your.email@example.com>
2024-10-10 19:54:50 +0900

    新規ユーザーアイコン追加画面

---
+++ app/createUser/icon.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import ImgPick from '../compData/imgPick';
+
+const Icon = () => {
+
+  return (
+    <View style={s.container}>
+
+      <Text style={s.mainTitle}>アイコンを選ぼう</Text>
+      <Text style={s.mainText}>自分の顔となる写真を選ぼう</Text>
+
+      <ImgPick/>
+
+    </View>
+  );
+};
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  mainTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '80%',
+  },
+  mainText: {
+    fontSize: 14,
+    width: '80%',
+    marginBottom: 50,
+  },
+});
+
+export default Icon;
