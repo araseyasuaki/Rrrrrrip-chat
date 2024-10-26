1303fe51b6a5e8db73ab4fa69f17b25fe81fbb31
Your Name <your.email@example.com>
2024-10-27 03:47:26 +0900

    ラストチェックコンポーネントのユーザー情報表示機能（UI込み）完成

--- app/createUser/lastCheck.jsx
+++ app/createUser/lastCheck.jsx
@@ -1,67 +1,117 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../recoil';
+import { getAuth } from '../firebase'
 
 const LastCheck = () => {
   const userData = useRecoilValue(userState);
+  const tags = userData.tagList.filter(tag => tag.selected);
+  const auth = getAuth();
+  const user = auth.currentUser;
+  console.log(userData)
 
   return (
-    <View style={styles.container}>
-        <Text style={styles.mainTitle}>最終確認</Text>
-        <Text style={styles.mainText}>最後にもう一度入力したプロフィールが間違っていないかを確認しよう！</Text>
+    <View style={s.container}>
+      <Text style={s.mainTitle}>最終確認</Text>
+      <Text style={s.mainText}>最後にもう一度入力したプロフィールが間違っていないかを確認しよう！</Text>
 
-        <View style={styles.profileContainer}>
-          <Image
-            style={styles.iconImg}
-            source={userData.imageUri ? { uri: userData.imageUri } : require('../../assets/images/iconImg.png')}
-          />
-          <Text style={styles.profileText}>名前: {userData.name}</Text>
-          <Text style={styles.profileText}>ユーザーID: {userData.userId}</Text>
-          <Text style={styles.profileText}>自己紹介: {userData.text}</Text>
-          {/* ダミーテキストで確認用 */}
-          <Text style={styles.profileText}>名前: {userData.name}</Text>
-          <Text style={styles.profileText}>ユーザーID: {userData.userId}</Text>
-          <Text style={styles.profileText}>自己紹介: {userData.text}</Text>
-          <Text style={styles.profileText}>名前: {userData.name}</Text>
-          <Text style={styles.profileText}>ユーザーID: {userData.userId}</Text>
-          <Text style={styles.profileText}>自己紹介: {userData.text}</Text>
-          <Text style={styles.profileText}>名前: {userData.name}</Text>
-          <Text style={styles.profileText}>ユーザーID: {userData.userId}</Text>
-          <Text style={styles.profileText}>自己紹介: {userData.text}</Text>
-          <Text style={styles.profileText}>名前: {userData.name}</Text>
-          <Text style={styles.profileText}>ユーザーID: {userData.userId}</Text>
-          <Text style={styles.profileText}>自己紹介: {userData.text}</Text>
-        </View>
+      <Image style={s.iconImg} source={{ uri: userData.imageUri }} />
+
+      <View style={s.nameContainer}>
+        <Text style={s.profileName}>{userData.name}</Text>
+        <View style={s.decBer} />
+        <Text style={s.profileId}>{userData.userId}</Text>
+      </View>
+
+      <View style={s.textContainer}>
+        <Text>{userData.text}</Text>
+      </View>
+
+      <View style={s.tagsBox}>
+        {tags.map(tag => (
+          <View key={tag.id} style={s.tagsContainer}>
+            <Image source={tag.imgUrl} style={s.tagImage} />
+            <Text style={s.tagText}>{tag.name}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
-    paddingBottom: 114,
+    alignItems: 'center',
+    width: '100%',
   },
   mainTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    width: '100%',
   },
   mainText: {
-    fontSize: 16,
-    marginBottom: 50,
-  },
-  profileContainer: {
-    alignItems: 'center',
-    marginBottom: 20,
+    fontSize: 14,
+    marginBottom: 30,
+    width: '100%',
   },
   iconImg: {
-    width: 180,
-    height: 180,
-    borderRadius: 90,
-    marginBottom: 50,
+    width: '45%',
+    maxWidth: 200,
+    aspectRatio: 1,
+    borderRadius: 100,
   },
-  profileText: {
-    fontSize: 18,
-    marginBottom: 10,
+  nameContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  profileName: {
+    fontSize: 16,
+  },
+  decBer: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#000',
+    marginVertical: 3,
+  },
+  profileId: {
+    fontSize: 14,
+  },
+  textContainer: {
+    backgroundColor: '#fff',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 13.5,
+    marginTop: 16,
+  },
+  tagsBox: {
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 11,
+  },
+  tagsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    borderRadius: 20,
+    padding: 6,
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderColor: '#000',
+    margin: 5,
+  },
+  tagImage: {
+    width: 20,
+    height: 20,
+  },
+  tagText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginLeft: 6,
   },
 });
 
