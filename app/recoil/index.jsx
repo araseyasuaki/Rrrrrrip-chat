e7e86e898857dc90395fb55cd7d8e075774d175e
Your Name <your.email@example.com>
2024-10-27 03:44:03 +0900

    リコイル関数のtagsを削除して、代わりにtagListを追加

--- app/recoil/index.jsx
+++ app/recoil/index.jsx
@@ -9,7 +9,13 @@
       name: '',
       userId: '',
       userIdFilter: '',
-      text: '',
-      tags: [],
+      text: 'よろしくお願いします！よろしくお願いします！よろしくお願いします！よろしくお願いします！よろしくお願いします！',
+      tagList: [
+        { id: 0, name: 'ポケモンGO', imgUrl: require(`../../assets/images/tagsIconA.png`), selected: false },
+        { id: 1, name: '原神', imgUrl: require(`../../assets/images/tagsIconB.png`), selected: false },
+        { id: 2, name: 'スターレイル', imgUrl: require(`../../assets/images/tagsIconC.png`), selected: false },
+        { id: 3, name: 'ゼンレスゾーンゼロ', imgUrl: require(`../../assets/images/tagsIconD.png`), selected: false },
+        { id: 4, name: 'サマナーズウォー', imgUrl: require(`../../assets/images/tagsIconE.png`), selected: false },
+      ],
     },
   });
