fc6755f3418428a2e8d82ff734d41a392f2cf63f
Your Name <23aw0103@jec.ac.jp>
2024-10-28 22:46:25 +0900

    画面旋回アニメーションの切り替え

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,9 +2,17 @@ import { Stack } from 'expo-router';
 import { RecoilRoot } from 'recoil';
 
 export default function RootLayout() {
+
+  const arase = false
+
   return (
     <RecoilRoot>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{
+        headerShown: false,
+        animation: arase ? 'slide_from_right' : 'slide_from_left', // araseがtrueなら右から左、falseなら左から右
+        gestureDirection: 'horizontal',
+        animationTypeForReplace: arase ? 'push' : 'pop',
+      }}>
         <Stack.Screen name="index" />
       </Stack>
     </RecoilRoot>
