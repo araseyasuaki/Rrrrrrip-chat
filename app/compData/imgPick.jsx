a94a22df02d03e745a595a04b2d81519546a3714
Your Name <23aw0103@jec.ac.jp>
2024-10-28 22:46:44 +0900

    imgPickのcss修正

--- app/compData/imgPick.jsx
+++ app/compData/imgPick.jsx
@@ -48,7 +48,7 @@ const s = StyleSheet.create({
   iconImg: {
     width: '100%',
     height: '100%',
-    borderRadius: '50%',
+    borderRadius: 90,
   },
   imgPulsBtn: {
     position: 'absolute',
